0d346e33a26fc0a94dd4db59fcc2bac1ae748929
naimur.naiyimu <naimur.naiyimu@gmail.com>
2024-10-13 17:22:14 +0600

    fix static

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 mkdir -p /opt/render/project/src/logs  # Create logs directory
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic
 python manage.py migrate
 if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
