1273c63ddf92fcd8e53e9ef89e3a3f7ba3af83b7
naimur.naiyimu <naimur.naiyimu@gmail.com>
2024-10-13 19:05:55 +0600

    Updated STATIC_ROOT to a writable directory

--- Config/settings/base.py
+++ Config/settings/base.py
@@ -59,7 +59,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Config.urls'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 TEMPLATES = [
     {
--- Config/settings/production.py
+++ Config/settings/production.py
@@ -42,8 +42,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
 STATIC_URL = '/static/'
-STATIC_ROOT = config('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = config('MEDIA_ROOT')
