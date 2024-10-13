6823c0d6ea091997e61bcac4f38ff50adf7bb456
naimur.naiyimu <naimur.naiyimu@gmail.com>
2024-10-13 21:14:29 +0600

    Updated STATIC_ROOT to a writable directory1.0.12

--- Config/custom_storage.py
+++ Config/custom_storage.py
@@ -1,10 +1,9 @@
 from whitenoise.storage import CompressedManifestStaticFilesStorage
-from django.contrib.staticfiles.storage import StaticFilesStorage
 
 class CustomStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    def _post_process(self, paths, dry_run=False, **options):
+    def _post_process(self, paths, dry_run=False, *args, **kwargs):
         try:
-            return super()._post_process(paths, dry_run, **options)
+            return super()._post_process(paths, dry_run, *args, **kwargs)
         except Exception as e:
             # Ignore the specific error for jquery-ui.css
             if "jquery-ui.css" in str(e):
