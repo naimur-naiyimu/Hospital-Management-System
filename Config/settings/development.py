f94d0dd505c431c1d8ef2ad3cfd531570a4d4d46
naimur.naiyimu <naimur.naiyimu@gmail.com>
2024-10-13 12:40:52 +0600

    updated STATIC_ROOT

--- Config/settings/development.py
+++ Config/settings/development.py
@@ -34,6 +34,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
